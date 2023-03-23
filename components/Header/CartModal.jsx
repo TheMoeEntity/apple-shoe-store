4062687bbcb3cec5d70b2c3920be1045e3cac451
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-03-23 10:41:02 +0100

    added cart button

--- components/Header/CartModal.jsx
+++ components/Header/CartModal.jsx
@@ -1,8 +1,11 @@
 import styles from "./Header.module.css";
 import Image from "next/image";
 import boy from "../../public/assets/shoegreen.jpeg";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const CartModal = ({ cartOpen }) => {
+    const router = useRouter()
   return (
     <div
       style={{ bottom: !cartOpen ? "100px" : "-500px" }}
@@ -34,7 +37,12 @@ const CartModal = ({ cartOpen }) => {
           <h4>$325.56</h4>
         </div>
         <p>shipping total calculated at checkout</p>
-        
+        <div className={styles.bottomControls}>
+          <Link href={"/cart"}>
+            <button>View cart</button>
+          </Link>
+          <button className={styles.checkBtn}>Check out</button>
+        </div>
       </div>
     </div>
   );
