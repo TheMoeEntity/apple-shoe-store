3c41503d7376874bd5a87da94408de9d6548f55c
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-04-18 16:09:20 +0100

    updated items section UI

--- components/Overview/index.jsx
+++ components/Overview/index.jsx
@@ -1,16 +1,46 @@
-import styles from './index.module.css'
-
+import styles from "./index.module.css";
+import { useEffect, useRef } from "react";
 const Overview = () => {
+    const container = useRef(null);
+    const hide = () => {
+        
+        let others = container.current.children;
+        for (const child of others) {
+          child.style.display = "none";
+        }
+        others[0].style.display = "block";
+
+      };
+      useEffect(() => {
+        hide();
+      }, []);
+      const displayContent = (index) => {
+        let others = container.current.children;
+        for (const child of others) {
+          child.style.display = "none";
+        }
+        others[index].style.display = "block";
+      };
   return (
     <div className={styles.panel}>
-        <div className={styles.controls}>
-            <div>Return policy</div>
-            <div>Shipping</div>
-            <div>Warranty</div>
-            <div>Reviews</div>
-        </div>
-    </div>
-  )
-}
+      <div className={styles.controls}>
+        <div onClick={() => displayContent(0)}>Return policy</div>
+        <div onClick={() => displayContent(1)}>Shipping</div>
+        <div onClick={() => displayContent(2)}>Warranty</div>
+        <div onClick={() => displayContent(3)}>Reviews</div>
+      </div>
+      <div ref={container} className={styles.content}>
+        <div>There are no returns or exchanges for this particular item</div>
 
-export default Overview
+        <div>
+          <i className="fa-solid fa-shipping-fast"></i> Delivery time <br />
+          {" Estimated delivery time: 1-5 working days"}
+        </div>
+
+        <div>Warranty information unavailable for this item.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Overview;
