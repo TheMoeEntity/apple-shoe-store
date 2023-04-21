b3e44d286b21b796dc8a8fb50aa2496db1540c6f
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2023-04-21 21:27:05 +0100

    changed layout.js for fontawesome

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import  Footer from "./Footer/Footer";
 import { useRouter } from "next/router";
 import Header from './Header/Header'
+import Script from 'next/script'
 export const Layout = ({ children }) => {
   const router = useRouter();
   return (
@@ -9,8 +10,8 @@ export const Layout = ({ children }) => {
       {children}
       {router.pathname == "/login" || router.pathname == "/signup" ? (<></>): (
         <>
-          <Footer />
-        
+        <Footer />
+        <Script src="https://kit.fontawesome.com/4ef8c63dd7.js" crossorigin="anonymous"></Script>
         </>
       )}
     </div>
