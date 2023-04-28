128e3557e16d1ca252d09aeffe0e92ae3fa98cb1
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-04-28 17:38:23 +0100

    changed 404 page design

--- components/Layout.js
+++ components/Layout.js
@@ -6,9 +6,9 @@ export const Layout = ({ children }) => {
   const router = useRouter();
   return (
     <div className="container">
-      <Header />
+      {router.pathname == "/404" ? (<></>):(<Header />)}
       {children}
-      {router.pathname == "/login" || router.pathname == "/signup" ? (<></>): (
+      {router.pathname == "/login" || router.pathname == "/signup" || router.pathname == "/404" ? (<></>): (
         <>
         <Footer />
         <Script src="https://kit.fontawesome.com/4ef8c63dd7.js" crossorigin="anonymous"></Script>
--- pages/404.jsx
+++ pages/404.jsx
@@ -1,11 +1,21 @@
-import styles from '../styles/404.module.css'
+import Link from "next/link";
+import Script from "next/script";
 
-const Error  = () => {
+const Error = () => {
   return (
-    <div className={styles.error}>
-        Page Not Found
-    </div>
-  )
-}
+    <>
+      <div id="notfound">
+        <div className="notfound">
+          <div className="notfound-404">
+            <h1>Oops!</h1>
+            <h2>404 - The Page can't be found</h2>
+          </div>
+          <Link href={`/`}>Go To Homepage</Link>
+        </div>
+        <Script src="https://kit.fontawesome.com/4ef8c63dd7.js" crossorigin="anonymous"></Script>
+      </div>
+    </>
+  );
+};
 
-export default Error
+export default Error;
